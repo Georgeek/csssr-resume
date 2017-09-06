4ac517c7a5430d6fde4d9f7a563eeb20540f4499
Egor Madiarov <Georgeek@users.noreply.github.com>
2017-09-06 21:58:53 +0800

    refactory 0.28

--- app/blocks/js-bar/js-bar.js
+++ app/blocks/js-bar/js-bar.js
@@ -1,10 +1,43 @@
-'use strict';
+const sliderElem = document.getElementById('slider');
+const thumbElem = sliderElem.children[0];
 
-$(document).on('ready', function() {
-	$( '#slider' ).slider({
-		value: 153,
-		min: 0,
-		max: 800,
-		step: 1
-	});
-});
+function getCoords(elem) {
+	const box = elem.getBoundingClientRect();
+	return {
+		top: box.top + pageYOffset,
+		left: box.left + pageXOffset
+	};
+}
+
+thumbElem.onmousedown = function (e) {
+	const thumbCoords = getCoords(thumbElem);
+	const	shiftX = e.pageX - thumbCoords.left;
+	const	sliderCoords = getCoords(sliderElem);
+
+	document.onmousemove = function (el) {
+		let newLeft = el.pageX - shiftX - sliderCoords.left;
+		const rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
+		if (newLeft < 0) {
+			newLeft = 0;
+		}
+
+		if (newLeft > rightEdge) {
+			newLeft = rightEdge;
+		}
+
+		thumbElem.style.left = newLeft + 'px';
+	};
+
+	document.onmouseup = () => {
+		document.onmousemove = document.onmouseup = null;
+	};
+
+	return false;
+};
+
+thumbElem.ondragstart = () => {
+	console.log('click DRAG');
+	return false;
+};
+
+
