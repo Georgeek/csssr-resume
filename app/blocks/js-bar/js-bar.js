4dc8d4fd0607cba2a17403dab421252ad1d04cfc
Egor Madiarov <Georgeek@users.noreply.github.com>
2017-10-20 15:57:43 +0800

    refactoring js 0.01

--- app/blocks/js-bar/js-bar.js
+++ app/blocks/js-bar/js-bar.js
@@ -1,43 +1,172 @@
-(function () {
-	const sliderElem = document.getElementById('slider');
-	const thumbElem = sliderElem.children[0];
+import $ from 'jquery';
 
-	function getCoords(elem) {
-		const box = elem.getBoundingClientRect();
-		return {
-			left: box.left + pageXOffset
-		};
-	}
+// Пример данных: [название, проценты]
+const datas = [
+	['Не владею'],
+	['Использую готовые решения', 19],
+	['Использую готовые решения и умею их переделывать', 48],
+	['Пишу сложный JS с нуля']
+];
 
-	thumbElem.onmousedown = function (e) {
-		const thumbCoords = getCoords(thumbElem);
-		const	shiftX = e.pageX - thumbCoords.left;
-		const	sliderCoords = getCoords(sliderElem);
+function Slider(options) {
+	const elem = options.elem;
+	const max = options.max || 100;
+	const data = options.data || null;
 
-		document.onmousemove = function (el) {
-		// вычесть координату родителя, т.к. position: relative
+	let sliderCoords, thumbCoords, shiftX;
 
-			let newLeft = el.pageX - shiftX - sliderCoords.left;
-			const rightEdge = sliderElem.offsetWidth - thumbElem.offsetWidth;
-			if (newLeft < 0) {
-				newLeft = -7;
+	// Создаем в контейнере линию и бегунок для слайдера
+	const thumbElem = line;
+	const line = document.createElement('div');
+	const thumb = document.createElement('div');
+	elem.appendChild(line);
+	line.setAttribute('id', 'line');
+	thumb.className = 'thumb';
+	line.appendChild(thumb);
+
+		// Здесь рисуются вехи скроллбара
+	(function() {
+		// Не рисуем скролбар, если в массиве всего 1 цифра. Иначе зачем нам рисовать?
+		if (data.length < 2) {
+			const span = document.createElement('div');
+			// прячем скроллбар
+			line.hidden = true;
+			// создаем элемент спан и вставляем его в id/класс --- переписать
+			elem.appendChild(span);
+			span.className = "span";
+			span.innerHTML = data[0][0];
+
+			// Рисуем, если в массиве 2 или более цифры
+		}else if (data.length >= 2) {
+			let i;
+			// Объявляем первый и последний элемент массива
+			const first = data[0][0];
+			const last = data[data.length - 1][0];
+			const firstTestEl = document.createElement('div');
+			const firstElement = `<div class="js-bar__text"> ${first} </div>`;
+			firstTestEl.innerHTML = firstElement;
+			firstTestEl.className = 'circle';
+			firstTestEl.setAttribute('id', 'circle0');
+			firstTestEl.setAttribute('style', 'left: 0%;');
+
+			// Рисуем начальную точку скролбара
+			line.appendChild(firstTestEl);
+
+			// Циклом проходим через промежуточные данные
+			for (i = 1; i < data.length - 1; i++) {
+				// Рисуем промежуточные данные, где data[i][1] - это проценты на которые элемент сместится в скроллбаре
+				$('#line').append(`
+					<div class='circle' id='circle${i}' style='left: ${data[i][1]}%;'>
+						<div class="js-bar__text"> ${data[i][0]} </div>
+					</div>
+				`);
 			}
 
-			if (newLeft > rightEdge) {
-				newLeft = rightEdge + 5;
-			}
-			thumbElem.style.left = newLeft + 'px';
-		};
+			// Рисуем конец скролбара
+			$('#line').append(`
+				<div class='circle' id='circle${i}' style='left: 99%;'>
+					<div class="js-bar__text"> ${last} </div>
+				</div>
+			`);
+		}
 
-		document.onmouseup = () => {
-			document.onmousemove = document.onmouseup = null;
-		};
+		$('.circle:nth-child(4)').addClass('active');
+	}());
 
-		return false;
-	};
+	// makeCircles();
 
-	thumbElem.ondragstart = () => {
-		console.log('click DRAG');
-		return false;
-	};
-}());
+	// const pixelPerValue = (elem.clientWidth - thumbElem.clientWidth) / max;
+
+	// elem.ondragstart = function () {
+	// 	return false;
+	// };
+
+	// function onDocumentMouseMove(e) {
+	// 	moveTo(e.clientX);
+	// }
+
+	// function onDocumentMouseUp() {
+	// 	endDrag();
+	// }
+
+	// function startDrag(startClientX) {
+	// 	thumbCoords = thumbElem.getBoundingClientRect();
+	// 	shiftX = startClientX - thumbCoords.left;
+
+	// 	sliderCoords = elem.getBoundingClientRect();
+
+	// 	document.addEventListener('mousemove', onDocumentMouseMove);
+	// 	document.addEventListener('mouseup', onDocumentMouseUp);
+	// }
+
+	// elem.onmousedown = function (event) {
+	// 	if (event.target.closest('.thumb')) {
+	// 		startDrag(event.clientX, event.clientY);
+
+	// 		// отключаем изменение курсора в начале выделения
+	// 		return false;
+	// 	}
+	// };
+
+	// function positionToValue(left) {
+	// 	return Math.round(left / pixelPerValue);
+	// }
+
+	// function moveTo(clientX) {
+	// 	// определяем координату родителя
+	// 	let newLeft = clientX - shiftX - sliderCoords.left;
+
+	// 	// если курсор ушел вне слайдера
+	// 	if (newLeft < 0) {
+	// 		newLeft = 0 - 7;
+	// 	}
+
+	// 	const rigthEdge = elem.offsetWidth - thumbElem.offsetWidth;
+	// 	if (newLeft > rigthEdge) {
+	// 		newLeft = rigthEdge + 5;
+	// 	}
+
+	// 	thumbElem.style.left = newLeft + 'px';
+
+	// 	elem.dispatchEvent(new CustomEvent('slide', {
+	// 		bubbles: true,
+	// 		detail: positionToValue(newLeft)
+	// 	}));
+	// }
+
+	// function endDrag() {
+	// 	document.removeEventListener('mousemove', onDocumentMouseMove);
+	// 	document.removeEventListener('mouseup', onDocumentMouseUp);
+
+	// 	elem.dispatchEvent(new CustomEvent('change', {
+	// 		bubbles: true,
+	// 		detail: positionToValue(parseInt(thumbElem.style.left, 10))
+	// 	}));
+	// }
+
+	// function valueToPosition(value) {
+	// 	return pixelPerValue * value;
+	// }
+
+	// function setValue(value) {
+	// 	thumbElem.style.left = valueToPosition(value) + 'px';
+	// }
+
+	// this.setValue = setValue;
+}
+
+const sliderElem = document.getElementById('lineCont');
+const slider = new Slider({
+	elem: sliderElem,
+	max: 5000,
+	data: datas
+});
+
+
+	// sliderElem.addEventListener('slide', function(event) {
+	// 	document.getElementById('slide').innerHTML = event.detail;
+	// });
+
+	// sliderElem.addEventListener('change', function(event) {
+	// 	document.getElementById('change').innerHTML = event.detail;
+	// });
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
 // Прежде чем включать этот скрипт, нужно в js-bar jade раскомментировать разметку
-// import '../blocks/js-bar/js-bar.js';
+import '../blocks/js-bar/js-bar.js';
 
-import '../blocks/js-bar/js-bar-test.js';
+// import '../blocks/js-bar/js-bar-test.js';
